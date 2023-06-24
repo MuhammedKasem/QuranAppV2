4bc3de77602fd859a416b83dc6f84ea5101d9102
Muhammed Kasem <muhammedkasem2002@gmail.com>
2023-06-24 16:19:44 -0400

    Fixed the rendering of QuranList to properly display correct surah

--- app/Surah/[id].js
+++ app/Surah/[id].js
@@ -1,17 +1,48 @@
-import React from 'react';
-import { Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl } from 'react-native'
-import {Stack, useRouter, useSearchParams} from "expo-router";
+import React, {useEffect} from 'react';
+import {Text, View, SafeAreaView, ScrollView, ActivityIndicator, RefreshControl, FlatList} from 'react-native'
+import quranData from '../../data/quran_en.json';
+import {Stack, useLocalSearchParams, useRouter, useSearchParams} from "expo-router";
 import { useCallback, useState} from 'react';
+import AppLoading from 'expo-app-loading';
 
 import { COLORS, icons, SIZES} from '../../constants';
 import useFetch from "../../hook/useFetch";
 
 
 const Surah = () => {
-return (
-    <Text>Working!</Text>
-)
+    const params = useLocalSearchParams();
+    const { id: idString } = params;
+    const id = Number(idString);
+
+    const [surahData, setSurahData] = useState(null);
+
+
+    useEffect(() => {
+        const surahDataFound = quranData.find((surah) => surah.id === id);
+        setSurahData(surahDataFound);
+    }, [id]);
+
+    const renderItem = ({ item: verse }) => (
+        <View>
+            <Text>
+                {verse.text}
+            </Text>
+            <Text>
+                {`${verse.id}. ${verse.translation}`}
+            </Text>
+        </View>
+    );
+    if (!surahData) {
+        return <ActivityIndicator/>;
+    }
+    return (
+
+        <FlatList
+            data={surahData.verses}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+/>
+    )
 }
 
 export default Surah;
-
--- components/quran/QuranList/QuranList.js
+++ components/quran/QuranList/QuranList.js
@@ -5,33 +5,33 @@ import styles from '../QuranList/QuranList.style'
 
 const QuranList = () => {
     const router = useRouter();
-
-     const arabicNumerals = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
+    const arabicNumerals = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'];
 
     const toArabicNumerals = (number) => {
         return [...number.toString()].map(digit => arabicNumerals[digit]).join('');
     }
 
     const handleSurahPress = (surah) => {
-        router.push(`/Surah/${surah.id}`)
+        router.push({pathname: `/Surah/${surah.id}`, params: {id: surah.id}})
+
     }
 
 
     const renderItem = ({ item: surah }) => (
         <TouchableOpacity onPress={() => {handleSurahPress(surah)}}>
-        <View style={styles.surah}>
-        <View style={styles.surahName}>
-            <Text style={styles.englishName}>
-                {`${surah.id} - ${surah.transliteration}`}
-            </Text>
-            <Text style={styles.arabicName}>
-                {`${toArabicNumerals(surah.id)} - ${surah.name}`}
-            </Text>
-        </View>
-            <View>
-                <Text style={styles.englishTranslation}>{surah.translation}</Text>
+            <View style={styles.surah}>
+                <View style={styles.surahName}>
+                    <Text style={styles.englishName}>
+                        {`${surah.id} - ${surah.transliteration}`}
+                    </Text>
+                    <Text style={styles.arabicName}>
+                        {`${toArabicNumerals(surah.id)} - ${surah.name}`}
+                    </Text>
+                </View>
+                <View>
+                    <Text style={styles.englishTranslation}>{surah.translation}</Text>
+                </View>
             </View>
-        </View>
         </TouchableOpacity>
     );
     return (
